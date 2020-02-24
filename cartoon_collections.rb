ceac3ee547ff35573084bdf90371de282c5edf43
Malcom Fetus <sora.vista@gmail.com>
2020-02-24 00:19:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+dwarves.each_with_index do |num, i|
+
+end
+empty_array
 end
 
 def summon_captain_planet# code an argument here
