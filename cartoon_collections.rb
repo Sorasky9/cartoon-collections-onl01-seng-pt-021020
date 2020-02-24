79cc5e233cba49311ec178adc4f042e5f18369cd
Malcom Fetus <sora.vista@gmail.com>
2020-02-24 00:31:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ dwarves.each_with_index do |num, i|
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+
+
 end
 
 def long_planeteer_calls# code an argument here
